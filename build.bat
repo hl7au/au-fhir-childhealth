76038ea98e385190bf4c0f7d07e48ca52b9fe625
ORIDASHI-SERVER\brett <brett.esler@oridashi.com.au>
2018-09-18 13:53:58 +1000

    APGAR score draft

--- build.bat
+++ build.bat
@@ -2,6 +2,6 @@
 
 echo You have to edit the path to the publisher...
 pause
-java -Xmx1024m -jar "C:\igpublisher\org.hl7.fhir.igpublisher.jar" -ig ig.json
+java.exe -Xmx1024m -jar "..\au-fhir-base\org.hl7.fhir.igpublisher.jar" -ig ig.json -watch
 
 pause
