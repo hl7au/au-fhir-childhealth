860cae07b5479e93f1d456fd2ca1c57b2c99b36e
NCDHC (Shovan Roy) <shovan.roy.newit@gmail.com>
2019-08-14 08:47:46 +1000

    Updated Sample for Birth View HI

--- build.bat
+++ build.bat
@@ -2,6 +2,7 @@
 
 echo You have to edit the path to the publisher...
 pause
-java.exe -Xmx4096m -jar "..\au-fhir-base-jar\r4\org.hl7.fhir.igpublisher.jar" -ig ig.json -watch
+
+java.exe -Xmx4096m -jar "..\au-fhir-base-jar\r4\org.hl7.fhir.publisher.jar" -ig ig.json -watch
 
 pause
