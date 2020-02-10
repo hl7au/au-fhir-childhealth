c725d6b12234709a4ad7c67b4de31a00351a8c53
NCDHC (Shovan Roy) <shovan.roy.newit@gmail.com>
2020-02-11 09:55:28 +1100

    DPHR IG Build Files. For Local Use Only

---
+++ build-dphr.bat
@@ -0,0 +1,26 @@
+@echo off 
+
+echo Clearing Files...
+
+cd output
+del *.* /s/ q
+cd..
+cd temp
+del *.* /s/ q
+cd..
+cd qa
+del *.* /s/ q
+cd..
+
+echo Clearing Cache...
+
+cd txCache
+del *.* /s/ q
+cd..
+
+echo Building profiles...
+
+
+java.exe -Xmx4096m -jar "..\au-fhir-base-jar\r4\org.hl7.fhir.publisher.jar" -ig ig-dphr.json -watch
+
+pause
